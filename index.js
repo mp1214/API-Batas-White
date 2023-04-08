ad042620ad3e39e322805e78a0b430054b878acf
mp1214 <madepato3@gmail.com>
2023-04-08 10:56:52 -0300

    se modifica db.json

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const server =jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port =process.env.PORT || 5000;
+var cors = require('cors');
+res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
 
 server.use(middlewares);
 server.use(router);
-
+app.use(cors());
 server.listen(port);
