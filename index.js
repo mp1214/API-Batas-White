a87272350e140bd4381ddecb1702dab6ed90e18d
mp1214 <madepato3@gmail.com>
2023-04-08 11:19:51 -0300

    se modifica index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const server =jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port =process.env.PORT || 5000;
-var cors = require('cors');
+
 
 server.use(middlewares);
 server.use(router);
-app.use(cors());
+
 server.listen(port);
